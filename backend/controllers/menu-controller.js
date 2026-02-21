56b6c60787f68219ef8179a1414c7651aa9dc690
Neftaly <neftalyrv23@gmail.com>
2026-02-21 13:28:49 -0400

    Created new files for controllers and routers

---
+++ backend/controllers/menu-controller.js
@@ -0,0 +1,8 @@
+export function createMenuController({ menuRepository }) {
+  return {
+    getMenu: async (req, res, next) => {
+      const menu = await menuRepository.getAll();
+      return res.json({ menu });
+    },
+  };
+}

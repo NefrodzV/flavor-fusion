94850bda4ef0177d034da58b27674d6eeba23512
Neftaly <neftalyrv23@gmail.com>
2026-02-21 15:21:20 -0400

    Setup app erro handler

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express'
+import { logError } from './utils/index.js'
 
 export function createApp({ menuRouter }) {
-  const app = express();
+    const app = express()
 
-  app.use("/api/menu", menuRouter);
+    app.use('/api/menu', menuRouter)
 
-  return app;
+    app.use((err, req, res, next) => {
+        logError(err)
+        return res.status(500).json({ error: 'Internal Server Error' })
+    })
+    return app
 }

098df8b382083ab4c8328e7f56e0e8d71112d4a6
Neftaly <neftalyrv23@gmail.com>
2026-02-21 20:19:44 -0400

    Wrote a condition to not log when in tests

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,7 +1,10 @@
+import { env } from 'node:process'
 export function log(text) {
+    if (env.NODE_ENV === 'test') return
     console.log(text)
 }
 
 export function logError(text) {
+    if (env.NODE_ENV === 'test') return
     console.error(text)
 }

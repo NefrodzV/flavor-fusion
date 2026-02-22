d5a3ee0f110d91a9a8d1305587d1c203ff75ff1d
Neftaly <neftalyrv23@gmail.com>
2026-02-21 20:20:17 -0400

    Added a todo comment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import app from './app.js'
 import { env } from 'node:process'
 import { log } from './utils/logger.js'
 const PORT = env.PORT || 3000
+/** TODO:
+ * Need to initialize routers and controllers here
+ */
 app.listen(PORT, () => {
     log(`Server is listening in http://localhost:${PORT}`)
 })

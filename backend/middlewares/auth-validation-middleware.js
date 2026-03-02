a06e0c5170bc90029afd638824a1d257b0f9b1ab
Neftaly <neftalyrv23@gmail.com>
2026-03-02 09:48:48 -0400

    Added a util function to see if a var is a string and changed the auth middleware conditions to use them. All test pass.

--- backend/middlewares/auth-validation-middleware.js
+++ backend/middlewares/auth-validation-middleware.js
@@ -1,4 +1,6 @@
+import { type } from 'node:os'
 import { DataIsInvalidError } from '../errors/data-is-invalid-error.js'
+import { isString } from '../utils/string.js'
 
 const EMAIL_REGEX =
     /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$/i
@@ -10,21 +12,37 @@ export function validateRegisterUser(req, _res, next) {
     const email = req.body?.email
     const password = req.body?.password
 
-    if (!name?.trim()) {
+    if (name == null) {
         errors.name = 'Name is required.'
+    } else if (!isString(name)) {
+        errors.name = 'Name must be a string type'
+    } else if (!name?.trim()) {
+        errors.name = 'Name is empty.'
     }
 
-    if (!lastName?.trim()) {
+    if (lastName == null) {
         errors.lastName = 'Last name is required.'
+    } else if (!isString(lastName)) {
+        errors.lastName = 'Last name must be a string type.'
+    } else if (!lastName?.trim()) {
+        errors.lastName = 'Last name is empty.'
     }
 
-    if (!email?.trim()) {
+    if (email == null) {
+        errors.email = 'Email is required.'
+    } else if (!isString(email)) {
+        errors.email = 'Email must be a string type.'
+    } else if (!email?.trim()) {
         errors.email = 'Email is required.'
     } else if (!EMAIL_REGEX.test(email)) {
         errors.email = 'Invalid email address.'
     }
 
-    if (!password?.trim()) {
+    if (password == null) {
+        errors.password = 'Password is required'
+    } else if (!isString(password)) {
+        errors.password = 'Password must be a string type.'
+    } else if (!password?.trim()) {
         errors.password = 'Password is required.'
     } else if (password.length < 8) {
         errors.password = 'Password must be at least 8 characters long.'

2d6a7155b7e6f79629619e26af5288c4cfb265dc
Neftaly <neftalyrv23@gmail.com>
2026-03-07 18:21:36 -0400

    Made query to get all menu and tested it

--- backend/db/repositories/menu-repository.js
+++ backend/db/repositories/menu-repository.js
@@ -1,1 +1,30 @@
-export async function getAll() {}
+export function createMenuRepository(db) {
+    if (!db) {
+        throw new Error('db param is undefined')
+    }
+    return {
+        getAll: async () => {
+            const { rows } = await db.query(`
+                SELECT
+                    mi.id,
+                    mi.name, 
+                    description,
+                    price_cents,
+                    json_agg(images.obj) as images
+                FROM menu_items mi
+                LEFT JOIN 
+                (
+                SELECT
+                    mii.menu_item_id,
+                    jsonb_build_object('storage_key',a.storage_key,'width',a.width, 'height',a.height, 'name', a.name) as obj
+                FROM menu_item_images mii
+                LEFT JOIN assets a ON a.id = mii.asset_id GROUP By mii.id, a.storage_key, a.width, a.height, a.name
+
+                ) images ON mi.id = images.menu_item_id
+                 GROUP BY mi.id
+            `)
+
+            return rows
+        },
+    }
+}
---
+++ backend/tests/unit/queries/menu.test.js
@@ -0,0 +1,13 @@
+import test from 'node:test'
+import assert from 'node:assert'
+
+import { createMenuRepository } from '../../../db/repositories/menu-repository.js'
+import { pool } from '../../../db/pool.js'
+
+test('getAll returns array of menu items', async () => {
+    const menuRepository = createMenuRepository(pool)
+    const res = await menuRepository.getAll()
+    console.log(res)
+    assert.ok(Array.isArray(res))
+    await pool.end()
+})

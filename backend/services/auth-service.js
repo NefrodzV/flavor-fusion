5f8e50c8860474e72bdb70f606fb820b20d78d3f
Neftaly <neftalyrv23@gmail.com>
2026-03-27 18:59:03 -0400

    Fixed bugs function calls without async,incorrecondition in existing value in login function and incorrect and incorrect placement for password compare hash function.

--- backend/services/auth-service.js
+++ backend/services/auth-service.js
@@ -1,5 +1,6 @@
 import { EmailAlreadyExistsError } from '../errors/email-already-exits-error.js'
 import { InvalidCredentialsError } from '../errors/incorrect-email-or-password-error.js'
+import { env } from '../config/env.js'
 export function createAuthService({
     userRepository,
     tokenService,
@@ -9,24 +10,30 @@ export function createAuthService({
         register: async ({ name, lastName, email, password }) => {
             const existing = await userRepository.findByEmail(email)
             if (existing) throw new EmailAlreadyExistsError()
-            const hashed = hashService.hash(password)
+            const hashed = await hashService.hash(password)
             const user = await userRepository.createUser(
                 name,
                 lastName,
                 email,
                 hashed
             )
-            const token = await tokenService.sign(user.id)
+            const token = await tokenService.sign(
+                { userId: user.id },
+                env.jwtSecret
+            )
             return { user, token }
         },
 
         login: async (email, password) => {
             const existing = await userRepository.findByEmail(email)
-            if (existing) throw new InvalidCredentialsError()
-            if (!(await hashService.compare(existing.password, password)))
+            if (!existing) throw new InvalidCredentialsError()
+            if (!(await hashService.compare(password, existing.password)))
                 throw new InvalidCredentialsError()
-            const token = tokenService.sign(existing.id)
-            return { user, token }
+            const token = await tokenService.sign(
+                { userId: existing.id },
+                env.jwtSecret
+            )
+            return { user: existing, token }
         },
 
         getCurrentUser: async (userId) => {

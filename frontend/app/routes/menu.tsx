cd423ca7fa4dbb166344181c3e82af71513028f0
Neftaly <neftalyrv23@gmail.com>
2026-08-30 18:37:58 -0400

    Made the base of menu text larger.

--- frontend/app/routes/menu.tsx
+++ frontend/app/routes/menu.tsx
@@ -3,7 +3,7 @@ import { DishGrid } from "~/components/menu/menu-grid";
 export default function Menu() {
   return (
     <main className="px-[10%] py-[5%]">
-      <h1 className="text-base font-bold md:text-xl lg:text-3xl text-center mb-2">
+      <h1 className="text-xl font-bold md:text-xl lg:text-3xl text-center mb-2">
         Menu
       </h1>
       <DishGrid />

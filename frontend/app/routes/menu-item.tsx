ae64bf420dc807a53b45c1f39128d183d9119a7f
Neftaly <neftalyrv23@gmail.com>
2026-09-08 10:38:16 -0400

    Fixed an issue with a possible null value in formattedImagesBySize

--- frontend/app/routes/menu-item.tsx
+++ frontend/app/routes/menu-item.tsx
@@ -11,39 +11,36 @@ import { MainLayout } from "~/components/layout/main-layout";
 export default function MenuItemPage() {
   const [menuItem, setMenuItem] = useState<MenuItem | null>(null);
   const params = useParams();
-  const [formattedImagesBySize, setFormattedImagesBySize] = useState<{
-    small?: MenuImage;
-    large?: MenuImage;
-  } | null>();
 
   useEffect(() => {
     async function getMenuItem() {
       const res = await fetch(`${env.apiUrl}/api/menu/${params.slug}`);
       const data: MenuItemResponse = await res.json();
-
-      let updatedFormattedImages: { small?: MenuImage; large?: MenuImage } = {};
-      data.menuItem.images.forEach((image: MenuImage) => {
-        if (image.width < 700) {
-          updatedFormattedImages["small"] = image;
-        } else {
-          updatedFormattedImages["large"] = image;
-        }
-      });
-      setFormattedImagesBySize(updatedFormattedImages);
       setMenuItem(data.menuItem);
     }
-
     getMenuItem();
   }, [params.slug]);
+
+  const formattedImagesBySize = menuItem?.images.reduce<{
+    small?: MenuImage;
+    large?: MenuImage;
+  }>((acc, image) => {
+    if (image.width < 700) {
+      acc["small"] = image;
+    } else {
+      acc["large"] = image;
+    }
+    return acc;
+  }, {});
   if (!menuItem) return <>Is Loading</>;
 
   return (
     <MainLayout>
       <h1 className="pbe-[.5rem] text-xl font-medium">{menuItem.name}</h1>
       <Image
-        src={`${env.apiUrl}/api/${formattedImagesBySize["small"]?.storage_key}/${formattedImagesBySize["small"]?.name}`}
-        srcSet={`${env.apiUrl}/api/${formattedImagesBySize["small"]?.storage_key}/${formattedImagesBySize["small"]?.name} 650w,
-              ${env.apiUrl}/api/${formattedImagesBySize["large"]?.storage_key}/${formattedImagesBySize["large"]?.name} 1920w`}
+        src={`${env.apiUrl}/api/${formattedImagesBySize?.small?.storage_key}/${formattedImagesBySize?.small?.name}`}
+        srcSet={`${env.apiUrl}/api/${formattedImagesBySize?.small?.storage_key}/${formattedImagesBySize?.small?.name} 650w,
+              ${env.apiUrl}/api/${formattedImagesBySize?.large?.storage_key}/${formattedImagesBySize?.large?.name} 1920w`}
         sizes="(max-width: 600px) 650px,
                1920px"
         css="rounded-sm aspect-3/2 object-cover"

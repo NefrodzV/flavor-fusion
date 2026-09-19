b510c2c68fabb29d4eab4704e5d31c62ac1c250b
Neftaly <neftalyrv23@gmail.com>
2026-09-19 18:22:56 -0400

    Update to have inline css

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-export function Header({ children }: { children: React.ReactNode }) {
+export function Header({
+  children,
+  css
+}: {
+  children: React.ReactNode;
+  css: string;
+}) {
   return (
-    <header className="px-[5%] bg-purple-200 min-h-[50px] flex items-center justify-between">
+    <header
+      className={`${css} px-[5%] bg-purple-200 min-h-[50px] flex items-center justify-between`}
+    >
       {children}
     </header>
   );

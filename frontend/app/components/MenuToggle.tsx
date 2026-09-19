2a9af289c653d6222203779ce07cabd2c9c6efc4
Neftaly <neftalyrv23@gmail.com>
2026-09-19 18:21:04 -0400

    Updated component for accesibility and inline css

--- frontend/app/components/MenuToggle.tsx
+++ frontend/app/components/MenuToggle.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 export function MenuToggle({
   isOpen,
-  onClick
+  onClick,
+  css
 }: {
   isOpen: boolean;
   onClick: React.Dispatch<React.SetStateAction<boolean>>;
+  css: string;
 }) {
   return (
     <button
+      aria-label="Toggle navigation menu"
+      aria-expanded={isOpen}
+      aria-controls="hamburger-menu"
       onClick={() => onClick((prev) => !prev)}
-      className="relative h-[30px] w-[35px] border-none bg-transparent p-0 cursor-pointer"
+      className={`${css} relative h-[30px] w-[35px] border-none bg-transparent p-0 cursor-pointer`}
     >
       <div
         className={`pointer-events-none absolute ${isOpen ? "top-1/2 rotate-45" : "top-0"} h-[4px] w-full rounded bg-black transition-all duration-500 ease-[cubic-bezier(0.8,0.5,0.2,1.4)]`}
